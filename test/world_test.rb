dfffe4d618106714559209ce4c3133afacd29793
Brandon Stewart <boveus@gmail.com>
2018-05-13 17:32:57 -0400

    dry up the world test

--- test/world_test.rb
+++ test/world_test.rb
@@ -13,6 +13,20 @@ class World::Minitest < Minitest::Test
     assert_nil world.row_three
   end
 
+  def sample_data
+      { id: 1,
+       generation_id: 1,
+       row_one: '101101011',
+       row_two: '110101011',
+       row_three: '010101011' }
+  end
+
+  def select_all_worlds
+    sqldb = SQLite3::Database.new("./db/world_database.db")
+    statement = 'SELECT * FROM worlds'
+    result = sqldb.execute(statement)
+  end
+
   # This was super tricky for me, so I will give you this one.
   # Your world should have an instance variable like the following:
   # @database = SQLite3::Database.new("./db/world_database.db")
@@ -28,43 +42,27 @@ class World::Minitest < Minitest::Test
   end
 
   def test_a_world_can_be_instantiated_with_data_from_a_hash
-    data = { id: 1,
-             generation_id: 1,
-             row_one: '101101011',
-             row_two: '110101011',
-             row_three: '010101011' }
-    world = World.new(data)
+    world = World.new(sample_data)
 
-    assert_equal data[:id], world.id
-    assert_equal data[:generation_id], world.generation_id
-    assert_equal data[:row_one], world.row_one
-    assert_equal data[:row_two], world.row_two
-    assert_equal data[:row_three], world.row_three
+    assert_equal sample_data[:id], world.id
+    assert_equal sample_data[:generation_id], world.generation_id
+    assert_equal sample_data[:row_one], world.row_one
+    assert_equal sample_data[:row_two], world.row_two
+    assert_equal sample_data[:row_three], world.row_three
   end
 
   def test_a_world_can_be_saved_to_the_database
     wdb = WorldDatabase.new
     wdb.setup('world_database')
-    data = { id: 1,
-             generation_id: 1,
-             row_one: '101101011',
-             row_two: '110101011',
-             row_three: '010101011' }
-    world = World.new(data)
-
-    sqldb = SQLite3::Database.new("./db/world_database.db")
-    statement = 'SELECT * FROM worlds'
-    result = sqldb.execute(statement)
+    world = World.new(sample_data)
+    result = select_all_worlds
 
     # There shouldn't be anything in the database until we call
     # save on the world we just created
     assert_equal [], result
 
     world.save
-
-    sqldb = SQLite3::Database.new("./db/world_database.db")
-    statement = 'SELECT * FROM worlds'
-    result = sqldb.execute(statement)
+    result = select_all_worlds
     wdb.drop('world_database')
 
     assert_equal [[1, 1, "101101011", "110101011", "010101011"]], result
@@ -76,18 +74,11 @@ class World::Minitest < Minitest::Test
   def test_a_world_can_be_created
     wdb = WorldDatabase.new
     wdb.setup('world_database')
-    data = { id: 1,
-             generation_id: 1,
-             row_one: '101101011',
-             row_two: '110101011',
-             row_three: '010101011' }
     # this method should also return the world object that is created
     # the world object being a plain ol' ruby object
-    world = World.create(data)
+    world = World.create(sample_data)
+    result = select_all_worlds
 
-    sqldb = SQLite3::Database.new("./db/world_database.db")
-    statement = 'SELECT * FROM worlds'
-    result = sqldb.execute(statement)
     wdb.drop('world_database')
 
     assert_equal 1, world.id
@@ -100,22 +91,16 @@ class World::Minitest < Minitest::Test
   def test_a_world_can_be_updated
     wdb = WorldDatabase.new
     wdb.setup('world_database')
-    data = { id: 1,
-             generation_id: 1,
-             row_one: '101101011',
-             row_two: '110101011',
-             row_three: '010101011' }
     updated_data = {
              row_one: '101100011',
              row_two: '111110111',
              row_three: '010000011' }
-    world = World.new(data)
+    world = World.new(sample_data)
     world.save
     world.update(updated_data)
 
-    sqldb = SQLite3::Database.new("./db/world_database.db")
-    statement = 'SELECT * FROM worlds'
-    result = sqldb.execute(statement)
+    result = select_all_worlds
+
     wdb.drop('world_database')
 
     assert_equal [[1, 1, '101100011', '111110111', '010000011']], result
@@ -126,22 +111,14 @@ class World::Minitest < Minitest::Test
   def test_a_world_can_be_destroyed
     wdb = WorldDatabase.new
     wdb.setup('world_database')
-    data = { id: 1,
-             generation_id: 1,
-             row_one: '101101011',
-             row_two: '110101011',
-             row_three: '010101011' }
-    world = World.new(data)
+    world = World.new(sample_data)
     world.save
-
-    sqldb = SQLite3::Database.new("./db/world_database.db")
-    statement = 'SELECT * FROM worlds'
-    result = sqldb.execute(statement)
+    result = select_all_worlds
 
     assert_equal [[1, 1, "101101011", "110101011", "010101011"]], result
 
     world.destroy
-    result = sqldb.execute(statement)
+    result = select_all_worlds
     wdb.drop('world_database')
 
     assert_equal [], result
